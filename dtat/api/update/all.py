680b08d1d40745fd9aa424bb5b5b2e08160d645f
Daniel Hampl <danhampl31415@gmail.com>
2019-05-01 19:14:18 +0200

    fix

--- dtat/api/update/all.py
+++ dtat/api/update/all.py
@@ -3,7 +3,7 @@ from dtat.services.update import updateAll
 from flask import jsonify
 
 
-@updateprint.route('/all', methods=['GET, PATCH'])
+@updateprint.route('/all', methods=['GET', 'PATCH'])
 def all():
     """
     Updates all guilds in database.
