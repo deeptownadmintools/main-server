8fe4cf80440e888e9d69b1d8543bfa436d67a77e
Daniel Hampl <danhampl31415@gmail.com>
2019-05-01 15:52:18 +0200

    not working

--- tests/00_integration/test_updateName.py
+++ tests/00_integration/test_updateName.py
@@ -27,54 +27,55 @@ data = {
 
 @mock.patch('dtat.services.rockbite.rockbite_guildByName.requests')
 def test_updateName(mReq, client, app, session):
-    res = len(client.get('/data/guild/all').get_json()['data'])
-    assert res == 0
-    guildCount = len(Guild.query.all())
+    # res = len(client.get('/data/guild/all').get_json()['data'])
+    # assert res == 0
+    assert len(Guild.query.all()) == 0
 
     mReq.get.return_value.json.return_value = data
     res = client.get('/data/update/name/test')
     assert res.status_code == 404
     assert res.get_json()['message'] == 'Api response was not ok.'
-    assert guildCount == len(Guild.query.all())
+    assert len(Guild.query.all()) == 0
 
     data['status'] = 'ok'
     mReq.get.return_value.json.return_value = data
     res = client.get('/data/update/name/test')
     assert res.get_json()['result'] == 'ok'
     assert res.status_code == 200
-
-    assert guildCount + 3 == len(Guild.query.all())
+    assert len(Guild.query.all()) == 3
 
     data['status'] = 'nok'
     mReq.get.return_value.json.return_value = data
     res = client.get('/data/update/name/test')
     assert res.status_code == 404
     assert res.get_json()['message'] == 'Api response was not ok.'
-
-
-@mock.patch('dtat.services.rockbite.rockbite_guildByName.requests')
-def test_updateName_2(mReq, client, app, session):
-    data['status'] = 'nok'
-    res = len(client.get('/data/guild/all').get_json()['data'])
-    assert res == 0
-    guildCount = len(Guild.query.all())
-
-    mReq.get.return_value.json.return_value = data
-    res = client.get('/data/update/name/test')
-    assert res.status_code == 404
-    assert res.get_json()['message'] == 'Api response was not ok.'
-    assert guildCount == len(Guild.query.all())
+    assert len(Guild.query.all()) == 3
 
     data['status'] = 'ok'
+    data["result"] = [
+        {
+            "guild_id": "1",
+            "guild_name": "Testtttttt",
+            "level": 10
+        },
+        {
+            "guild_id": "2",
+            "guild_name": "testytesty",
+            "level": 0
+        },
+        {
+            "guild_id": "3",
+            "guild_name": "test",
+            "level": 0
+        },
+        {
+            "guild_id": "4",
+            "guild_name": "test",
+            "level": 0
+        },
+    ]
     mReq.get.return_value.json.return_value = data
     res = client.get('/data/update/name/test')
     assert res.get_json()['result'] == 'ok'
     assert res.status_code == 200
-
-    assert guildCount + 3 == len(Guild.query.all())
-
-    data['status'] = 'nok'
-    mReq.get.return_value.json.return_value = data
-    res = client.get('/data/update/name/test')
-    assert res.status_code == 404
-    assert res.get_json()['message'] == 'Api response was not ok.'
+    assert len(Guild.query.all()) == 4
